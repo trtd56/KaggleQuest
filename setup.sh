712ecc230b2aa951cdc4f1732d75928cf79b1ba0
Your Name <you@example.com>
2020-01-28 04:36:15 +0000

    little fix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 pip install pandas sklearn torch matplotlib
 
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
 git clone https://github.com/huggingface/transformers > /dev/null
 cd transformers && pip install . > /dev/null && cd ..
 rm -rf transformers
